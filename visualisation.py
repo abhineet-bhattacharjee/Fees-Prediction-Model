81e7c01c86195ec8cea659e61f9fc6049d5fffc9
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-14 18:22:11 +0530

    feat: add fees vs freq graph

--- visualisation.py
+++ visualisation.py
@@ -7,4 +7,6 @@ df = pd.read_csv('Harvard Fees Dataset.csv')
 
 sns.histplot(df['cost'], bins=20)
 plt.title('Distribution of Tuition Fees')
+plt.xlabel('Tuition Fees')
+plt.ylabel('Frequency')
 plt.show()
