cfdffeb9553e9c4e378883f4e6a6654d510ee940
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-14 18:15:07 +0530

    fix: fix module import issue

---
+++ visualisation.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+df = pd.read_csv('Harvard Fees Dataset.csv')
+
+sns.histplot(df['cost'], bins=20)
+plt.title('Distribution of Tuition Fees')
+plt.show()
