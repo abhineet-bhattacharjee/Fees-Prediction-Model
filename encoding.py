caca54a66f1cf502acb561346c2e09fe6cac029b
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-15 00:07:26 +0530

    feat: add one-hat encoding for schools

--- encoding.py
+++ encoding.py
@@ -1,8 +1,10 @@
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 from visualisation import df
+
 
 X = df[['academic.year', 'school']]
 y = df['cost']
@@ -12,4 +14,10 @@ ct = ColumnTransformer(
         ('school_enc', OneHotEncoder(drop='first', sparse_output=False), ['school'])
     ],
     remainder='passthrough'
+)
+
+X_encoded = ct.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_encoded, y, test_size=0.2, random_state=42
 )
