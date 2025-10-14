1ecd2db472ef01c9ce0d7fe0b80713e4eea95ab5
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-14 23:47:31 +0530

    chore: define transformer logic for df['school']

--- encoding.py
+++ encoding.py
@@ -3,3 +3,13 @@ from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 
 from visualisation import df
+
+X = df[['academic.year', 'school']]
+y = df['cost']
+
+ct = ColumnTransformer(
+    transformers=[
+        ('school_enc', OneHotEncoder(drop='first', sparse_output=False), ['school'])
+    ],
+    remainder='passthrough'
+)
