2a2e45ed1a759634e9d68272495c3f9efaa0e1e8
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-14 23:43:06 +0530

    chore: transfer all graphs into respective functions

--- visualisation.py
+++ visualisation.py
@@ -5,21 +5,29 @@ import pandas as pd
 
 df = pd.read_csv('Harvard Fees Dataset.csv')
 
-sns.histplot(df['cost'], bins=20)
-plt.title('Cost vs Frequency Graph')
-plt.xlabel('Tuition Fees')
-plt.ylabel('Frequency')
-plt.show()
+def cost_vs_frequency():
+    sns.histplot(df['cost'], bins=20)
+    plt.title('Cost vs Frequency Graph')
+    plt.xlabel('Tuition Fees')
+    plt.ylabel('Frequency')
+    plt.show()
 
-sns.scatterplot(data=df, x='academic.year', y='cost', hue='school')
-plt.title('Year vs Cost Graph')
-plt.xlabel('Academic Year')
-plt.ylabel('Tuition Fees')
-plt.show()
+def year_vs_cost():
+    sns.scatterplot(data=df, x='academic.year', y='cost', hue='school')
+    plt.title('Year vs Cost Graph')
+    plt.xlabel('Academic Year')
+    plt.ylabel('Tuition Fees')
+    plt.show()
 
-sns.boxplot(data=df, x='school', y='cost')
-plt.title('School vs Cost Graph')
-plt.xlabel('School')
-plt.ylabel('Tuition Fees')
-plt.xticks(rotation=45)
-plt.show()
+def school_vs_cost():
+    sns.boxplot(data=df, x='school', y='cost')
+    plt.title('School vs Cost Graph')
+    plt.xlabel('School')
+    plt.ylabel('Tuition Fees')
+    plt.xticks(rotation=45)
+    plt.show()
+
+if __name__ == '__main__':
+    cost_vs_frequency()
+    year_vs_cost()
+    school_vs_cost()
