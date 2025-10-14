9f726f2a3d0b4562ec8fb2dd8392c2e77bfaf495
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-14 18:29:40 +0530

    feat: add year vs fees graph

--- visualisation.py
+++ visualisation.py
@@ -6,7 +6,13 @@ import pandas as pd
 df = pd.read_csv('Harvard Fees Dataset.csv')
 
 sns.histplot(df['cost'], bins=20)
-plt.title('Distribution of Tuition Fees')
+plt.title('Cost vs Frequency Graph')
 plt.xlabel('Tuition Fees')
 plt.ylabel('Frequency')
+plt.show()
+
+sns.scatterplot(data=df, x='academic.year', y='cost', hue='school')
+plt.title('Year vs Cost Graph')
+plt.xlabel('Academic Year')
+plt.ylabel('Tuition Fees')
 plt.show()
