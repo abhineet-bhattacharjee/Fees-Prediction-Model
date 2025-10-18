62c0e3c2c4af67de4ee664bfb382ebb22faea384
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-18 15:57:36 +0530

    feat: add lasso_regression() testing model

--- model_selection.py
+++ model_selection.py
@@ -8,6 +8,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import Ridge, Lasso, LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.exceptions import ConvergenceWarning
 import numpy as np
 import pandas as pd
 
@@ -20,6 +21,7 @@ X_test = pd.DataFrame(X_test, columns=['academic.year', 'school'])
 
 warnings.filterwarnings(action='ignore', category=UserWarning, message='Singular matrix in solving dual problem.')
 warnings.filterwarnings(action='ignore', category=RuntimeWarning, message='Ill-conditioned matrix')
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 def evaluation(y_true, y_pred, tolerance=0.1):
     y_true, y_pred = np.array(y_true), np.array(y_pred)
@@ -42,9 +44,27 @@ def ridge_regression(degree):
     ridge_acc = evaluation(y_test, ridge_pred)
     results['Ridge Regression'] = {'MAE': ridge_mae, 'R2': ridge_r2, 'Accuracy (within 10%)': ridge_acc, 'Degree': degree}
 
+def lasso_regression(degree):
+    lasso_pipe = Pipeline([
+        ('preprocess', ct),
+        ('poly', PolynomialFeatures(degree=degree, include_bias=False)),
+        ('model', Lasso(max_iter=10000))
+    ])
+    lasso_params = {'model__alpha': [0.01, 0.1, 1, 10]}
+    lasso_grid = GridSearchCV(lasso_pipe, lasso_params, cv=5,
+                              scoring='neg_mean_absolute_error', n_jobs=-1)
+    lasso_grid.fit(X_train, y_train)
+    lasso_best = lasso_grid.best_estimator_
+    lasso_pred = lasso_best.predict(X_test)
+    lasso_mae = mean_absolute_error(y_test, lasso_pred)
+    lasso_r2 = r2_score(y_test, lasso_pred)
+    lasso_acc = evaluation(y_test, lasso_pred)
+    results['Lasso Regression'] = {'MAE': lasso_mae, 'R2': lasso_r2, 'Accuracy (within 10%)': lasso_acc, 'Degree': degree}
+
 
 if __name__ == '__main__':
     for degree in [1, 2, 3]:
         ridge_regression(degree)
+        lasso_regression(degree)
     results_sorted = dict(sorted(results.items(), key=lambda x: x[1]['MAE']))
     pprint(results_sorted)
