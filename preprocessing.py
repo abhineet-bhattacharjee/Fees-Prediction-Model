2dab8b421d6dbe770b0cc95412d9789f199db972
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-21 23:05:14 +0530

    feat: add data-preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,6 @@
 import pandas as pd
+
+df = pd.read_csv('Harvard Fees Dataset.csv')
+pivot_df = df.pivot(index='academic.year', columns='school', values='cost')
+pivot_df.reset_index(inplace=True)
+pivot_df.to_csv('dataset.csv', index=False)
