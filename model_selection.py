03e707047813ced278b4719275ad3daa24501012
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-22 00:03:28 +0530

    add: add temporary warning overrides

--- model_selection.py
+++ model_selection.py
@@ -10,3 +10,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.linear_model import Ridge, Lasso, LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+
+warnings.filterwarnings('ignore', category=UserWarning, message='Singular matrix in solving dual problem')
+warnings.filterwarnings('ignore', category=RuntimeWarning, message='Ill-conditioned matrix')
+warnings.filterwarnings('ignore', category=ConvergenceWarning)
