bd1948037129c914859f70fc4ba899cb2d07a499
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-22 00:02:15 +0530

    chore: import necessary modules

---
+++ model_selection.py
@@ -0,0 +1,12 @@
+from pprint import pprint
+import warnings
+import numpy as np
+import pandas as pd
+
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.linear_model import Ridge, Lasso, LinearRegression
+from sklearn.ensemble import RandomForestRegressor
