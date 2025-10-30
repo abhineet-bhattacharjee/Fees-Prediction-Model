4c5cf70a0f5bc59eaa0fc7e2f77a1932608d15c3
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2025-10-30 17:22:33 +0530

    add: add scenarios

--- model_deployment.py
+++ model_deployment.py
@@ -1,3 +1,8 @@
 import subprocess
 
 schools = ['Business (MBA)', 'Law', 'Medical/Dental', 'Divinity']
+scenarios = [
+    {'inflation': None, 'endowment': None, 'name': 'baseline'},
+    {'inflation': 3.5, 'endowment': 50.0, 'name': 'moderate'},
+    {'inflation': 5.5, 'endowment': 60.0, 'name': 'high_growth'},
+]
